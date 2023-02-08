641de5b7664886fb63969e85b2d0bd31debba4ee
Rubén Martínez <rumama02@floridauniversitaria.es>
2023-02-08 18:37:14 +0100

    Este es el ejercicio 1 del grupo 1 dificil

---
+++ ejer1grupo1.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+if [ $# -ne 1 ] || [ $1 -le 0 ];then
+	echo "Debes ejecutar el script como: $0 n"
+	echo "Donde n es un numero mayor que 0"
+	exit
+fi
+
+while read nombre; do 
+	mkdir $nombre
+	x=1
+	while [ $X -le $1 ]; do
+	mkdir "$nombre/personal $X"
+	X=$((X+1))
+	done
+done < nombres.txt
